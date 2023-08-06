68dd3c6fb58548b6394657ed5d3e04b8219fdf3a
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2023-08-06 19:52:26 +0200

    Update snake_game.py

--- mini_proyectos/snake_game/snake_game.py
+++ mini_proyectos/snake_game/snake_game.py
@@ -1,83 +1,76 @@
-import math
-import random
-import pygame
-import time
-import tkinter as tk
-from tkinter import messagebox
+from random import randrange
+import pygame as pg
 
-class Cube(object):
-    rows = 0
-    w = 0
+WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
+snake_dir = (0, 0)
+time, time_step = 0, 110
+food = snake.copy()
+food.center = get_random_position()
+screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
 
-class Snake:
-    def __init__(self, color, poss) -> None:
-        self.turns = {}
-        self.color = color
-        self.head = Cube(poss)
-        self.body = []
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w and snake_dir != (0, TILE_SIZE):
+                snake_dir = (0, -TILE_SIZE)
+            
+            if event.key == pg.K_s and snake_dir != (0, -TILE_SIZE):
+                snake_dir = (0, TILE_SIZE)
+            
+            if event.key == pg.K_a and snake_dir != (TILE_SIZE, 0):
+                snake_dir = (-TILE_SIZE, 0)
+            
+            if event.key == pg.K_d and snake_dir != (-TILE_SIZE, 0):
+                snake_dir = (TILE_SIZE, 0)
+
+    screen.fill("black")
+
+    self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
+
+    if snake.left <= -TILE_SIZE:
+        snake.center = (WINDOW + TILE_SIZE // 2, snake.centery)
     
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-            keys = pygame.key.get_pressed()
-
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                if keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                if keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                if keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-class Grid:
-    def __init__(self, size = 500, rows = 20) -> None:
-        self.screen = pygame.display.set_mode((size, size))
-        self.rows = rows
-        self.size = size
+    elif snake.right > WINDOW + TILE_SIZE:
+        snake.center = (-(TILE_SIZE // 2), snake.centery)
     
-    def redraw_window(self):
-        self.screen.fill((0, 0, 0))
-        self.draw_grid()
-        pygame.display.update()
+    if snake.top < -TILE_SIZE:
+        snake.center = (snake.centerx, WINDOW + TILE_SIZE // 2,)
     
-    def draw_grid(self):
-        square_dim = self.size // self.rows
-        x = 0
-        y = 0
+    elif snake.bottom > WINDOW + TILE_SIZE:
+        snake.center = (snake.centerx, -(TILE_SIZE // 2))
+    
+    if self_eating:
+        snake.center = get_random_position()
+        food.center = get_random_position()
+        length = 1
+        snake_dir = (0, 0)
+        segments = [snake.copy()]
 
-        for _ in range(self.rows):
-            x += square_dim
-            y += square_dim
-            pygame.draw.line(self.screen, (255, 255, 255), (x, 0), (x, self.size))
-            pygame.draw.line(self.screen, (255, 255, 255), (0, y), (self.size, y))
+    if food.center == snake.center:
+        food.center = get_random_position()
+        length += 1
 
-def main():
-    screen = Grid()
-    # snake = Snake((255, 0, 0), (10, 10))
-    clock = pygame.time.Clock()
-    flag = True
+    pg.draw.rect(screen, "red", food)
+    [pg.draw.rect(screen, "green", segment) for segment in segments] #Dibujar la serpiente
 
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        screen.redraw_window()
+    time_now = pg.time.get_ticks()
 
-main()
+    if time_now - time > time_step:
+        time = time_now
+        snake.move_ip(snake_dir)
+        segments.append(snake.copy())
+        segments = segments[-length:]
+
+    pg.display.flip()
+    clock.tick(60)
